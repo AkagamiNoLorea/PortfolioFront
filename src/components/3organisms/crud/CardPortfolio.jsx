06a2c3b8499eddf3b5d1b67da62094a5554acff7
AkagamiNoLorea <raiseta92@gmail.com>
2023-09-16 12:53:21 +0200

    style part 1

--- src/components/3organisms/crud/CardPortfolio.jsx
+++ src/components/3organisms/crud/CardPortfolio.jsx
@@ -1,24 +1,35 @@
-export default function CardPortfolio (props) {
-    
+import './CardPortfolio.css'
+export default function CardPortfolio(props) {
+
     const portfolio = props.portfolio;
-    
+
     return (
         <>
-        <div className="card">
-            
-            <div className="contenedor-datos">
-                <h3>{portfolio.nombre}</h3>
-                <p>{portfolio.descripcion}</p>
-                <img src ={portfolio.img} alt="" className="img"/> 
-                <p>{portfolio.lenguaje}</p>
-                <p>{portfolio.enlace}</p>
-                <p>{portfolio.despliegue}</p>
+            <div className="card">
+
+                <div className="card-container">
+                    <div class="card-text">
+                        <h2>{portfolio.nombre}</h2>
+                        <p>{portfolio.descripcion}</p>
+                    </div>
+                    <div class="card-image">
+                        <img src={portfolio.img} alt="" className="img" />
+                    </div>
+                    <div class="card-lenguajes">
+                            <p>{portfolio.lenguaje}</p>
+                            <div class="value">32</div>
+                            <div class="type">comments</div>
+                    </div>
+                    <div class="card-urls">
+                        <p>Enlace a GitHub: {portfolio.enlace}</p>
+                        <p>Despliegue: {portfolio.despliegue}</p>
+                    </div>
+                    <div className= "card-buttons">
+                        <button onClick={() => props.editPortfolio(portfolio)}> Editar </button>
+                        <button onClick={() => props.deletePortfolio(portfolio)}> Eliminar </button>
+                    </div>
+                    </div>
             </div>
-            <div className="buttons">
-                <button onClick={() => props.editPortfolio(portfolio) }> Editar </button>
-                <button onClick={() => props.deletePortfolio(portfolio) }> Eliminar </button>
-            </div>
-        </div>
         </>
     )
 }
