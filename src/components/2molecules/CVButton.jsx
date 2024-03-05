08681e2c0c4a5840f703861a0268c1410ea63dfe
Aida <128890496+AkagamiNoLorea@users.noreply.github.com>
2024-03-05 18:35:36 +0100

    Updated CV.jsx

--- src/components/2molecules/CVButton.jsx
+++ src/components/2molecules/CVButton.jsx
@@ -3,7 +3,7 @@ import './CVButton.css'
 const CVButton = () => {
   return (
     <>
-    <button className='download-button'><a href='src\assets\CurriculumAidaHerraiz.pdf' download>Descargar mi curriculum</a></button>
+    <button className='download-button'><a href='src\assets\CVAidaHerraiz.pdf' download>Descargar mi curriculum</a></button>
     </>
   )
 }
